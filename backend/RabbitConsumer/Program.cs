6d7bc8347b2770bb01294d07b470ffff4abdf9ec
Ruan de Jongh <ruandejongh2@outlook.com>
2023-10-17 21:32:10 +0200

    d

--- backend/RabbitConsumer/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using RabbitMQ.Client;
-
-var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
-factory.ClientProvidedName = "Rabbit sender App";
-
-IConnection cnn = factory.CreateConnection(); 
